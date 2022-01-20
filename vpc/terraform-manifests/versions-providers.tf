e84a455c0557746b43cca03036590515889ac236
Sohel Zaman <szaman05@yahoo.com>
2022-01-19 20:41:12 -0600

    created 1st vpc and provider

--- vpc/terraform-manifests/versions-providers.tf
+++ vpc/terraform-manifests/versions-providers.tf
@@ -9,6 +9,16 @@ terraform {
 }
 
 # Provider Block
+# The default provider configuration; resources that begin with `aws_` will use
+# it as the default, and it can be referenced as `aws`.
 provider "aws" {
-  region  = "us-east-1"
+  region = "us-east-1"
+  
 }
+
+# Additional provider configuration for west coast region; resources can
+# reference this as `aws.west`.
+# provider "aws" {
+#   alias  = "west"
+#   region = "us-west-2"
+# }
