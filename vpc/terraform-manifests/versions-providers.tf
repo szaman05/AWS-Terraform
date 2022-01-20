6663d1d9e274d77e480cb6590d3590920eb71b6d
Sohel Zaman <szaman05@yahoo.com>
2022-01-19 19:45:18 -0600

    1st initialization

---
+++ vpc/terraform-manifests/versions-providers.tf
@@ -0,0 +1,14 @@
+# Terraform Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-east-1"
+}
