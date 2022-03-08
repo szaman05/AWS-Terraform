05e18da28eb62197cb8dfc63bc2ddf392ed7a5c3
Sohel Zaman <szaman05@yahoo.com>
2022-03-08 12:11:52 -0600

    Create security_group.tf

---
+++ security_group.tf
@@ -0,0 +1,47 @@
+resource "aws_security_group" "ansible-sg" {
+  name        = "ansible-control-sg"
+  description = "Ansible Controller Default Security Group"
+
+  ingress {
+    description = "Allow Port 22 on My IPv4"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["72.180.39.175/32"]
+  }
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "web-db-sg" {
+  name        = "web-db-default-sg"
+  description = "Web VPC Default Security Group"
+
+  ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = "aws_security_group.ansible-sg.id"
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all IP and Ports Outbound"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
