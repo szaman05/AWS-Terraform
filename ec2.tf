06d3a14546f203c5c5cfa54fa2c146f436298c8a
Sohel Zaman <szaman05@yahoo.com>
2022-03-08 12:34:47 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,17 @@
 # Create EC2 Instance
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 
-resource "aws_instance" "my-ec2-vm" {
-  ami               = "ami-047a51fa27710816e"
+resource "aws_instance" "Ansible-Control" {
+  #ami               = "ami-047a51fa27710816e"
+#Ubuntu Market Place Image for Ansible.
+  ami               = "ami-0056dae7008bc574b"
   instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
-  #availability_zone = "us-east-1b"
   tags = {
-    "Name" = "web"
-    #"tag1" = "Update-test-1"    
+    "Name" = "Ansible-Control"   
   }
+}
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  security_group_id    = aws_security_group.ansible-sg.id
+  network_interface_id = aws_instance.Ansible-Control.primary_network_interface_id
 }
